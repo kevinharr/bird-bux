a02baa92ef401c6785b08263d44894d07266f052
kevinharr <kevinharr@verizon.net>
2022-12-19 11:37:57 -0500

    setup window.onload function and init function.

--- js/app.js
+++ js/app.js
@@ -10,3 +10,13 @@ const messageEl = document.getElementById("message")
 const resetBtnEl = document. getElementById("reset-button")
 
 /*-------------------------------- Functions --------------------------------*/
+
+window.onload = init()
+
+function init() {
+    board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = false
+    tie = false
+}
+
