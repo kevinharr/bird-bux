47fc4096cd45cdbe7d39ab90af2a39a084a76317
kevinharr <kevinharr@verizon.net>
2022-12-19 11:30:20 -0500

    setup Cached Element References

--- js/app.js
+++ js/app.js
@@ -5,4 +5,8 @@ let board, turn, winner, tie, round
 
 /*------------------------ Cached Element References ------------------------*/
 
+const itemEls = docuemnt.querySelectorAll(".sqr")
+const messageEl = document.getElementById("message")
+const resetBtnEl = document. getElementById("reset-button")
+
 /*-------------------------------- Functions --------------------------------*/
