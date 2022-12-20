a0ace52d3be43257d6cf88d7b3a3dabd69e47a14
kevinharr <kevinharr@verizon.net>
2022-12-19 21:56:36 -0500

    added category buttons

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ const itemEls = document.querySelectorAll(".sqr")
 const messageEl = document.getElementById("message")
 const resetBtnEl = document. getElementById("reset-button")
 const quizCategories = [movies, music, travel, art]
+const questions = []
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -45,16 +46,38 @@ function handleClickCategory(evt) {
     const sqIdx = evt.target.id.replace("sq", "")
     if (winner === true){
       return
-    } else if {
-    placePiece(sqIdx)
-    checkForTie ()
-    checkForWinner ()
-    switchPlayerTurn ()
-    render()
+    } else if (Number(sqIdx) === 0) {
+        console.log("console log for movies", Number(sqIdx))
+        movies()
+    } else if (Number(sqIdx) === 1) {
+        music()
+    } else if (Number(sqIdx) === 2) {
+        travel()
+    } else if (Number(sqIdx) === 2) {
+        art()
     }
-  }
 
+function movies () {
 
+}
+//     placePiece(sqIdx)
+//     checkForTie ()
+//     checkForWinner ()
+//     switchPlayerTurn ()
+//     render()
+//     }
+//   }
+
+  function updateBoard() {
+    board.forEach(function(element, index){
+        if (element === 1) {
+            itemEls[index].textContent = "C"
+        } else if (element === null) {
+            itemEls[index].textContent = ""
+        } 
+    }        
+)
+}
 
 function render() {
     updateBoard()
@@ -69,114 +92,114 @@ function attachingListeners() {
 
 }
 
-function handleClick(evt) {
-    const sqIdx = evt.target.id.replace("sq", "")
-    if (board[Number(sqIdx)] !== null) {
-      return
-    } 
-    else if (winner === true){
-      return
-    } else {
-    placePiece(sqIdx)
-    checkForTie ()
-    checkForWinner ()
-    switchPlayerTurn ()
-    render()
-    }
-  }
-  function handleClick(evt) {
-    const sqIdx = evt.target.id.replace("sq", "")
-    if (board[Number(sqIdx)] !== null) {
-      return
-    } 
-    else if (winner === true){
-      return
-    } else {
-    placePiece(sqIdx)
-    checkForTie ()
-    checkForWinner ()
-    switchPlayerTurn ()
-    render()
-    }
-  }
+// function handleClick(evt) {
+//     const sqIdx = evt.target.id.replace("sq", "")
+//     if (board[Number(sqIdx)] !== null) {
+//       return
+//     } 
+//     else if (winner === true){
+//       return
+//     } else {
+//     placePiece(sqIdx)
+//     checkForTie ()
+//     checkForWinner ()
+//     switchPlayerTurn ()
+//     render()
+//     }
+//   }
+//   function handleClick(evt) {
+//     const sqIdx = evt.target.id.replace("sq", "")
+//     if (board[Number(sqIdx)] !== null) {
+//       return
+//     } 
+//     else if (winner === true){
+//       return
+//     } else {
+//     placePiece(sqIdx)
+//     checkForTie ()
+//     checkForWinner ()
+//     switchPlayerTurn ()
+//     render()
+//     }
+//   }
 
-function updateBoard() {
-    board.forEach(function(element, index){
-        if (element === 1) {
-            itemEls[index].textContent = "C"
-        } else if (element === null) {
-            itemEls[index].textContent = ""
-        } 
-    }        
-)
-}
+// function updateBoard() {
+//     board.forEach(function(element, index){
+//         if (element === 1) {
+//             itemEls[index].textContent = "C"
+//         } else if (element === null) {
+//             itemEls[index].textContent = ""
+//         } 
+//     }        
+// )
+// }
 
-function updateMessage() {
+// function updateMessage() {
   
-    let PlayerSelected
-    let PlayerOne = "Player One"
-    let PlayerTwo = "Player Two"
+//     let PlayerSelected
+//     let PlayerOne = "Player One"
+//     let PlayerTwo = "Player Two"
    
-    if (turn === 1) {
-      PlayerSelected = PlayerOne
-    } else if (turn === -1) {
-      PlayerSelected = PlayerTwo
-    }
-    if (winner === false && tie === false) {
-      messageEl.textContent = `It is the turn of ${PlayerSelected}.`
-    } else if (winner === false && tie === true) {
-      messageEl.textContent = "It is a tie."
-    } else if (winner === true && tie === false) {
-      messageEl.textContent = `The winner is ${PlayerSelected}.`
-    }
-  }
+//     if (turn === 1) {
+//       PlayerSelected = PlayerOne
+//     } else if (turn === -1) {
+//       PlayerSelected = PlayerTwo
+//     }
+//     if (winner === false && tie === false) {
+//       messageEl.textContent = `It is the turn of ${PlayerSelected}.`
+//     } else if (winner === false && tie === true) {
+//       messageEl.textContent = "It is a tie."
+//     } else if (winner === true && tie === false) {
+//       messageEl.textContent = `The winner is ${PlayerSelected}.`
+//     }
+//   }
 
-  function handleClick(evt) {
-    const sqIdx = evt.target.id.replace("sq", "")
-    if (board[Number(sqIdx)] !== null) {
-      return
-    } 
-    else if (winner === true){
-      return
-    } else {
-    placePiece(sqIdx)
-    checkForTie ()
-    checkForWinner ()
-    switchPlayerTurn ()
-    render()
-    }
-  }
+//   function handleClick(evt) {
+//     const sqIdx = evt.target.id.replace("sq", "")
+//     if (board[Number(sqIdx)] !== null) {
+//       return
+//     } 
+//     else if (winner === true){
+//       return
+//     } else {
+//     placePiece(sqIdx)
+//     checkForTie ()
+//     checkForWinner ()
+//     switchPlayerTurn ()
+//     render()
+//     }
+//   }
 
-  function placePiece(index) {
-    board[index] = turn
-  }
+//   function placePiece(index) {
+//     board[index] = turn
+//   }
 
-  function checkForTie() {
-    if (board.includes(null)) {
-      return
-    } else {
-      tie = true
-    }
-  }
+//   function checkForTie() {
+//     if (board.includes(null)) {
+//       return
+//     } else {
+//       tie = true
+//     }
+//   }
 
-  function checkForWinner() {
-    for (let i = 0; i < winningCombos.length; i++) {
-      let sum = 0
-      for (let c = 0; c < 3; c++) {
-        sum += board[winningCombos[i][c]]
-        if (Math.abs(sum) === 3)  {
-          winner = true
-        }
-      }   
-    }
-  }
+//   function checkForWinner() {
+//     for (let i = 0; i < winningCombos.length; i++) {
+//       let sum = 0
+//       for (let c = 0; c < 3; c++) {
+//         sum += board[winningCombos[i][c]]
+//         if (Math.abs(sum) === 3)  {
+//           winner = true
+//         }
+//       }   
+//     }
+//   }
 
-  function switchPlayerTurn() {
-    if (winner === true) {
-      return
-    } else {
-    turn = turn * -1
-  }
-  }
+//   function switchPlayerTurn() {
+//     if (winner === true) {
+//       return
+//     } else {
+//     turn = turn * -1
+//   }
+//   }
 
-  resetBtnEl.addEventListener('click', init)
+//   resetBtnEl.addEventListener('click', init)
